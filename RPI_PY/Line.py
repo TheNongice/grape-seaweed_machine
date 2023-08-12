3c520ce6d3fef5f4392b0fbaa76411c0e5ec4681
TheNongice Wasawat <ice7533678@gmail.com>
2023-08-12 23:56:38 +0700

    Add RPi Checker Status

--- RPI_PY/Line.py
+++ RPI_PY/Line.py
@@ -1,15 +1,11 @@
 import requests
 
 class LINE:
-    def __init__(self, token='svdr3xFLuKKbVhrD1wW0UyQrb4t1CBMR6mjyN9xfay8'):
-        print("HELLO IT ME!")
+    def __init__(self, token : str):
         self.token = token
         self.endpoint = "https://notify-api.line.me/api/notify"
         self.headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + self.token}
     
-    def send(self,text):
+    def send(self,text : str):
         r = requests.post(self.endpoint, headers=self.headers, data={'message': text})
         return (r.text,r.status_code)
-    
-    def __del__(self):
-        print("LINE Object has been destroyed")
--- RPI_PY/main.py
+++ RPI_PY/main.py
@@ -1,41 +1,38 @@
+import os
 import serial
 import time
-import pandas as pd
+import pandas as pd 
 import numpy as np
 import RPi.GPIO as GPIO
 import requests
 from Line import LINE
+from dotenv import load_dotenv
 
-line_msg = LINE()
+# Setup Mode (Value & Tokens)
+load_dotenv()
+LINE_TOKEN = os.getenv('LINE_TOKEN')
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
+exit_pg = 0
 
-# Test Relay
-for x in range(3):
-    GPIO.output(11,GPIO.HIGH)
-    l = line_msg.send('NOW GPIO.HIGH')
-    print(l[0])
-    time.sleep(1)
-    GPIO.output(11,GPIO.LOW)
-    l = line_msg.send('NOW GPIO.LOW')
-    print(l[0])
-    time.sleep(1)
+# Setup Mode (Class Instances)
+line_msg = LINE(LINE_TOKEN)
+
+def decode_text(text: str):
+    enc = text.decode('utf-8')
+    enc = enc.strip('\r\n')
+    return enc
+
+if __name__ == "__main__" and exit_pg == 0:
+    ser = serial.Serial('/dev/ttyACM0',9600)
+    while True:
+        data = ser.readline()
+        data = decode_text(data)
+        print(data)
+        time.sleep(2)
+        if data == "N_WATER":
+            line_msg.send('Change Water')
+        else:
+            line_msg.send('Test Service')
+
 GPIO.cleanup()
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- VISUAL --                                                                                                                             27        1,1           All
-
